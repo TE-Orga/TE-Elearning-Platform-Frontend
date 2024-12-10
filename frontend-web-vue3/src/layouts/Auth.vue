a05e8ace2cfadebb970d9a3a09c3bd7d2251b27e
yousraMsaouri <yousramsaouri13@gmail.com>
2024-12-10 11:45:33 +0100

    transfert des fichiers

---
+++ frontend-web-vue3/src/layouts/Auth.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <navbar />
+    <main>
+      <section class="relative w-full h-full py-40 min-h-screen">
+        <div
+            class="absolute top-0 w-full h-full bg-blueGray-200"
+        ></div>
+        <router-view />
+        <footer-small absolute />
+      </section>
+    </main>
+  </div>
+</template>
+<script>
+import Navbar from "../components/Navbars/navbar.vue";
+
+export default {
+   components: {
+    Navbar,
+  },
+};
+</script>
